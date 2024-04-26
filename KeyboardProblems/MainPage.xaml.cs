e9e2a0f34c1456064fcf5eaf0d7c40fb01aaa310
mer <mer@spyro-soft.com>
2024-04-26 17:29:54 +0200

    add projects with sample

---
+++ KeyboardProblems/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace KeyboardProblems;
+#if __IOS__
+using CoreGraphics;
+using Foundation;
+using UIKit;
+#endif
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+#if __IOS__
+        UIKeyboard.Notifications.ObserveWillShow((s, e) =>
+        {
+            NSValue result = (NSValue)e.Notification.UserInfo.ObjectForKey(new NSString(UIKeyboard.FrameEndUserInfoKey));
+            CGSize keyboardSize = result.RectangleFValue.Size;
+            keyboardView.Margin = new Thickness(0, 0, 0, (keyboardSize.Height));
+        });
+        UIKeyboard.Notifications.ObserveWillHide((s, e) =>
+        {
+            keyboardView.Margin = new Thickness(0);
+        });
+#endif
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
+
---
+++ KeyboardProblems/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KeyboardProblems;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+

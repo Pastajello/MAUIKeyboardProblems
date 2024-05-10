b5fd3b40d375b696056c3999bd4704834e88c9ea
mer <mer@spyro-soft.com>
2024-05-10 09:40:26 +0200

    debug

--- KeyboardProblems/MainPage.xaml.cs
+++ KeyboardProblems/MainPage.xaml.cs
@@ -2,11 +2,17 @@
 #if __IOS__
 using CoreGraphics;
 using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
 using UIKit;
 #endif
 public partial class MainPage : ContentPage
 {
     int count = 0;
+
+
 
     public MainPage()
     {
@@ -16,25 +22,50 @@ public partial class MainPage : ContentPage
         {
             NSValue result = (NSValue)e.Notification.UserInfo.ObjectForKey(new NSString(UIKeyboard.FrameEndUserInfoKey));
             CGSize keyboardSize = result.RectangleFValue.Size;
-            keyboardView.Margin = new Thickness(0, 0, 0, (keyboardSize.Height));
+            scrollView.Content.Margin = new Thickness(0, 0, 0, (keyboardSize.Height - 97));
+            var a = this.Handler.PlatformView as Microsoft.Maui.Platform.ContentView;
+
+            int asd = 3;
         });
         UIKeyboard.Notifications.ObserveWillHide((s, e) =>
         {
-            keyboardView.Margin = new Thickness(0);
+            scrollView.Content.Margin = new Thickness(0);
         });
 #endif
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        {
+#if IOS || MACCATALYST
+
+            var stack = new VerticalStackLayout()
+            {
+                BackgroundColor = Colors.Red,
+                Padding = new Thickness(32, 8, 32, 24),
+                HeightRequest = 97,
+                WidthRequest = App.Current.MainPage.Width,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            stack.Add(new Button()
+            {
+                HorizontalOptions = LayoutOptions.End,
+                Text = "Save"
+            });
+            var uiView = new UIView() { BackgroundColor = Colors.Gray.ToPlatform() };
+            uiView.Frame = new CGRect(0, 0, App.Current.MainPage.Width, 97);
+            //uiView.Items = new UIBarButtonItem[] { cancel, Title, save };
+            uiView.AddSubview(stack.ToPlatform(this.Handler.MauiContext));
+            uiView.AutoresizingMask = UIViewAutoresizing.FlexibleHeight;
+            handler.PlatformView.InputAccessoryView = uiView;
+            // handler.PlatformView.InputAccessoryView = stack.ToPlatform(this.Handler.MauiContext);
+#endif
+        });
+
+
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
     }
 }
 
